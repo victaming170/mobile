17958d4bfc5001eed1c280d72ff7ffb50c7bdae4
victaming170 <victaming17@gmail.com>
2021-01-15 16:59:23 +0800

    upgrade

--- fmt_iq00.py
+++ fmt_iq00.py
@@ -1,59 +1,53 @@
 
-# get input file
+file_in_pre = 'iof/f_test.bin'
+print("功能：二进制文件每两bit后添加2位0：iqiq --> iq00iq00")
+
+# input file name
 while True:
     file_in = input('Input file: ')
     if not file_in:
-        file_in = 'iof/f_test.bin'
+        file_in = file_in_pre
     try:
         with open(file_in, 'rb') as ftest:
             break
     except FileNotFoundError:
         print('Can not find the file, try again, pls.')
 
+# output file name
 for c in reversed(range(len(file_in))):
     if file_in[c] == '.':
         file_out = file_in[:c] + '_iq00' + file_in[c:]
         break
 with open(file_out, 'wb') as f_flush:
     print(f'Output file: {file_out}')
-
 fo = open(file_out, 'ab')
 
-# trans bytes to binary
-# new_byte_list = []
+# handle
+rd_n = 0
+MB_n = 0
 with open(file_in, 'rb') as fi:
-    rd_n = 0
-    MB_n = 0
     while True:
         rd_1B = fi.read(1)
         if not rd_1B:
-            print(f'Read Done. {rd_n}B.')
             break
         rd_n = rd_n + 1
-        if rd_n %1048576 == 0:      # 1MB
+        if rd_n %1048576 == 0:      # display per 1MB
             MB_n = MB_n + 1
-            print(f'{MB_n}MB,')
+            print(f'>{MB_n} MB')
         int_1B = int(rd_1B.hex(), 16)
         # print('byte{:0>2d}: 0x{:0>2X}'.format(rd_n, int_1B))
         # separate 4bit|4bit
         byte_h4bit = (int_1B >> 4) &0x0f
         byte_l4bit = int_1B &0x0f
-        # 0biqiq --> 0biq00_iq00
+        # 0bxxxx --> 0bxx00_xx00
         bh_x1B = ((byte_h4bit << 4) ^(byte_h4bit << 2)) &0b11001100
         bl_x1B = ((byte_l4bit << 4) ^(byte_l4bit << 2)) &0b11001100
         # print('byte{:0>2d}: 0x{:0>2x}\n{:0>4b}_{:0>4b} --> {:0>8b}_{:0>8b}'\
         #     .format(rd_n, int_1B, byte_h4bit, byte_l4bit, bh_x1B, bl_x1B))
         fo.write(bytes([bh_x1B]))
         fo.write(bytes([bl_x1B]))
-        # new_byte_list.append(bh_x1B)
-        # new_byte_list.append(bl_x1B)
+    print(f'Done. Sum = {rd_n} B.')
 
-# write in file
-# with open(file_out, 'wb') as fo:
-#     for new_byte in new_byte_list:
-#         fo.write(bytes([new_byte]))
-#     len_nbl = len(new_byte_list)
-#     print(f'Write Done. {len_nbl}B. Output file: {file_out}')
 fo.close()
 
-print('E N D\n')
+end = input('\n==== END ====\nEnter anything to exit...')
--- format_xchar.py
+++ format_xchar.py
@@ -1,6 +1,6 @@
 
-file_in_pre = 'iof/f_test_nobracket.txt'
-print("功能：在文本文件的每一行首尾加字符。")
+file_in_pre = 'iof/f_test_less8.txt'
+print("功能：补齐文本文档的每行的字符数。")
 
 # input file name
 while True:
@@ -13,13 +13,31 @@ while True:
     except FileNotFoundError:
         print(f'Can not find the "{file_in}", try again, pls.')
 
-bs = input('String added at the beginning of each line: ')
-es = input('String added at the end of each line(except the last line): ')
+# get char
+while True:
+    c_pl = input("The char needed to add(default'0'): ")
+    if not c_pl:
+        c_pl = 0
+    c_pl = str(c_pl)
+    if len(c_pl) == 1:
+        break
+    print('Single char pls.')
+
+# get how many chars per line
+while True:
+    n_pl = input("The num_per_line(default8): ")
+    if not n_pl:
+        n_pl = 8
+    try:
+        n_pl = int(n_pl)
+        break
+    except ValueError:
+        print('Integer pls.')
 
 # output file name
 for c in reversed(range(len(file_in))):
     if file_in[c] == '.':
-        file_out = file_in[:c] + '_brkt' + file_in[c:]
+        file_out = file_in[:c] + '_' + str(n_pl) + 'x' + c_pl + file_in[c:]
         break
 with open(file_out, 'w') as f_flush:
     print(f'Output file: {file_out}')
@@ -27,6 +45,7 @@ fo = open(file_out, 'a')
 
 # handle
 line_n = 0
+# spy_step = 1000
 line_k = 0
 with open(file_in, 'r') as fi:
     while True:
@@ -37,12 +56,15 @@ with open(file_in, 'r') as fi:
         if line_n %1000 == 0:
             line_k = line_k + 1
             print(f'>{line_k}kilo rows.')       # display per 1000 rows
-        if line[-1] != '\n':
-            line = bs + line
+        if line[-1] == '\n':
+            n_pl_real = n_pl + 1
         else:
-            line = bs + line[:-1] + es + '\n'
+            n_pl_real = n_pl
+        if len(line) < n_pl_real:
+            line = (n_pl_real - len(line)) * c_pl + line
         fo.write(line)
     print(f'Done. Sum = {line_n} rows.')
+
 fo.close()
 
 end = input('\n==== END ====\nEnter anything to exit...')
