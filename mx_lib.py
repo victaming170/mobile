e211bf9187a1380eba91205b47a6b07e957b3747
victaming170 <victaming17@gmail.com>
2021-03-02 20:29:35 +0800

    update for mx_lib

--- mx_lib.py
+++ mx_lib.py
@@ -32,21 +32,23 @@ def gen_out_file_name(file_in_, begin_='', end_=''):
         print("The output file and input file can't be named in same name.")
 
 
-# show all options, get the choice of user. descrip_=[str], default_=[int], options_=n*[str]
+# show all options, get the choice of user. descrip_=[str], default_=[int], options_=n*[str] or 1*list
 def option_button(descrip_, default_, *options_):
     # show the option description
     print(descrip_)
+    if len(options_) == 1 and type(options_[0]) == list:
+        options_ = options_[0]
     options_n = len(options_)
     # get default choice
     try:
         default_ = int(default_)
     except ValueError:
         default_ = 0
-        print('[ERROR-option_button]Invalid default option, reset to 0.')
+        print('>[ERROR-option_button]Invalid default option, reset to 0.')
     else:
         if default_ >= options_n:
             default_ = 0
-            print('[ERROR-option_button]Invalid default option, reset to 0.')
+            print('>[ERROR-option_button]Invalid default option, reset to 0.')
     # show options
     for n in range(options_n):
         if n != default_:
@@ -55,12 +57,12 @@ def option_button(descrip_, default_, *options_):
             print(f'[{n}] {options_[n]}')
     # get choice of user
     while True:
-        button = input('>>>')
+        button = input('>>> ')
         if not button:
             button = default_
             break
         elif button in [str(n) for n in range(options_n)]:
             break
         else:
-            print('Invalid option, try again pls.')
+            print('>Invalid option, try again pls.')
     return int(button)
