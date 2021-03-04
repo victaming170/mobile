8bd760f21271aa7a20158cf77222b3d90a18df6a
victaming170 <victaming17@gmail.com>
2021-03-04 22:16:54 +0800

    update

--- bin_to_text.py
+++ bin_to_text.py
@@ -1,37 +1,91 @@
-from mx_lib import get_filename
+from pathlib import Path
 
-print("Tran binary to hex_text_file.")
-
-file_in = get_filename('Input file', 'iof/f_test2.bin')
-file_out = str(file_in)[:-len(file_in.suffix)] + '.txt'
-print(f'Output file: {file_out}')
-
-# =========== main ==================
-fout = open(file_out, 'w')
-word = [0, 1, 2, 3]
-reading = True
-word_count = 0
-mb_count = 0
-with open(file_in, 'rb')as fin:
-    while reading:
-        for byte in range(4):
-            data = fin.read(1)
-            if data:
-                word[byte] = data.hex().zfill(2)
-            else:
-                word[byte] = '00'
-                reading = False
-        if reading:
-            out_word = '0x' + word[3] + word[2] + word[1] + word[0] + ',\n'
+# ============= import function from mx_lib===================
+# display_=[str], dir_mode_=[bool], default_=[str(path)]
+def get_filename(display_, default_='', dir_mode_=False):
+    while True:
+        print(display_)
+        str_in = input('>>> ')
+        if not str_in:
+            str_in = default_
+        file_Path = Path(str_in)
+        if dir_mode_ and file_Path.is_dir():
+            return file_Path
+        elif file_Path.is_file():
+            return file_Path
         else:
-            out_word = '0x' + word[3] + word[2] + word[1] + word[0]
-        fout.write(out_word)
-        # monitor
-        word_count += 1
-        if word_count == 0x40000:
-            mb_count += 1
-            print(f'\r> {mb_count} MB')
-            word_count = 0
+            print(f'Can not find {file_Path}, try again pls.')
 
-fout.close()
-print('==== End ====')
+# =============================================================
+# ==================== code body ==============================
+# =============================================================
+welcome = '= Tran binary_file to text_file. ='
+print(len(welcome) *'=')
+print(welcome)
+print(len(welcome) *'=')
+
+# config
+file_out_total = 5      # how many output file
+out_suffix = '.coe'     # suffix of output file
+size_of_line = 4        # how many bytes in every line, unit = Byte.
+
+file_out_beginning = 'memory_initialization_radix = 16;\nmemory_initialization_vector = \n'
+file_out_end = ';'
+
+line_beginning = ''
+line_end = ',\n'
+
+monitor_line_node = 256    # progress bar, show current progress every [%] lines
+line_limit = 1024           # when line_counter come to line_limit, close current output file
+
+# get input file
+file_in = get_filename('Input file', 'iof/f_test.bin')      # type(file_in) = Class Path
+fin = open(file_in, 'rb')
+
+# main loop
+for file_out_i in range(file_out_total):
+    # get new output file
+    file_out = str(file_in)[:-len(file_in.suffix)] + '-' + str(file_out_i) + out_suffix
+    print(f'Output file {file_out_i}: {file_out}')
+    fout = open(file_out, 'w')
+    # write at the very beginning
+    fout.write(file_out_beginning)
+
+    line_byte_list = size_of_line *[0]
+    working = True
+    line_cnt = 0
+    node_cnt = 0
+    while working:
+        for byte_n in range(size_of_line):
+            one_byte = fin.read(1)
+            if one_byte:
+                line_byte_list[byte_n] = one_byte.hex().zfill(2)
+            else:
+                # end of read
+                line_byte_list[byte_n] = '00'
+                working = False
+                print('End of reading.')
+        output_line = line_beginning
+        for i in reversed(range(size_of_line)):
+            output_line += line_byte_list[i]
+        # monitor
+        line_cnt += 1
+        if line_cnt %monitor_line_node == 0:
+            node_cnt += 1
+            print(f'> {node_cnt} *{monitor_line_node} lines')
+        # check end of write
+        if line_cnt == line_limit:
+            working = False
+            print('End of writing.')
+        # write in file
+        if working:
+            output_line += line_end
+        else:
+            output_line += file_out_end
+        fout.write(output_line)
+    # one output file ready
+    fout.close()
+    print()
+
+fin.close()
+print('======== End ========')
--- mx_lib.py
+++ mx_lib.py
@@ -1,38 +1,36 @@
 from pathlib import Path
 
 
-# display_=[str], dir_mode_=[bool], default_=[str(path)]
+# display_=[str], dir_mode_=[bool], default_=[str(path)], return=[class Path]
 def get_filename(display_, default_='', dir_mode_=False):
     while True:
         print(display_)
-        str_in = input('>>>')
+        str_in = input('>>> ')
         if not str_in:
             str_in = default_
-        # py_style_path = (repr(str_in).replace(r'\\', '/'))[1:-1]
         file_Path = Path(str_in)
         if dir_mode_ and file_Path.is_dir():
             return file_Path
         elif file_Path.is_file():
             return file_Path
         else:
-            print(f'Can not find {file_Path}, try again pls.')
+            print(f'>Can not find {file_Path}, try again pls.')
 
 
-# generate output file name. file_in_=[Path], begin_=[str], end_=[str]
-def gen_out_file_name(file_in_, begin_='', end_=''):
-    if begin_ or end_:
-        last_dot_i = file_in_.name.rfind('.')
+# generate output file name. file_in_=[class Path], begin_=[str], end_=[str], suffix_=[str], return=[class Path]
+def output_file_name(file_in_, begin_='', end_='', suffix_=''):
+    if begin_ or end_ or(suffix_ != file_in_.suffix):
         file_in_dir = '\\'.join(file_in_.parts[:-1])
         if file_in_dir:
             file_in_dir = file_in_dir + '\\'
-        file_in_name = file_in_.name[:last_dot_i] if last_dot_i != -1 else file_in_.name
-        file_out_ = file_in_dir + begin_ + file_in_name + end_ + file_in_.suffix
+        file_in_pure_name = file_in_.name[:-len(file_in_.suffix)]
+        file_out_ = file_in_dir + begin_ + file_in_pure_name + end_ + suffix_
         return Path(file_out_)
     else:
-        print("The output file and input file can't be named in same name.")
+        print(">The output file and input file can't be named in the same name.")
 
 
-# show all options, get the choice of user. descrip_=[str], default_=[int], options_=n*[str] or 1*list
+# show all options, get the choice of user. descrip_=[str], default_=[int], options_=n*[str] or 1*list, return=[int]
 def option_button(descrip_, default_, *options_):
     # show the option description
     print(descrip_)
